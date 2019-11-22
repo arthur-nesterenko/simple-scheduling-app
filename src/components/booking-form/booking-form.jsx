2d819d71963f3f88b08369395b249bf3639d3285
Arthur <nesterenko19@gmail.com>
2019-11-22 20:53:00 +0200

    :lipstick: refactoring booking form
    move main logic to base-form
    make booking form as abstraction over base form

--- src/components/booking-form/booking-form.jsx
+++ src/components/booking-form/booking-form.jsx
@@ -1,47 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Button, Form, Input, InputGroup, InputGroupAddon } from 'reactstrap';
 import { useBookingProvider } from './../../context/booking-provider';
+import BaseForm from 'components/base-form';
 
-
-const BookingForm = ( { onSubmit } ) => {
+const BookingForm = ( props ) => {
     const { hasEmail } = useBookingProvider();
-    const [values, setValues] = React.useState( null );
-    const handleSubmit = React.useCallback( ( e ) => {
-        e.preventDefault();
-
-        onSubmit( values );
 
 
-    }, [hasEmail, onSubmit, values] );
+    const isEmailExist = values => values && hasEmail( values.email );
 
-    const emailExist = values && hasEmail( values.email );
 
-    const onChange = React.useCallback( e => {
-        const target = e.target;
-        setValues( prev => ({
-            ...prev,
-            [ target.name ]: target.value,
-        }) );
-    }, [] );
-
-    return <Form onSubmit={handleSubmit}>
-        <InputGroup>
-            <Input type='email'
-                   name='email'
-                   onChange={onChange}
-                   invalid={emailExist}
-                   valid={values ? !hasEmail( values.email ) : undefined}
-                   placeholder="Please enter your email"/>
-            <InputGroupAddon addonType="append">
-                <Button disabled={emailExist} color="secondary">Confirm</Button>
-            </InputGroupAddon>
-        </InputGroup>
-    </Form>;
+    return <BaseForm {...props} disableButton={isEmailExist}
+                     invalidInput={isEmailExist}
+                     validInput={v => !isEmailExist( v )}/>;
 };
 
 
+BookingForm.defaultProps = {
+    buttonName: 'Confirm',
+    buttonColor: 'secondary',
+};
+
 BookingForm.propTypes = {
     onSubmit: PropTypes.func,
+    buttonName: PropTypes.string,
+    buttonColor: PropTypes.string,
 };
 export default BookingForm;
