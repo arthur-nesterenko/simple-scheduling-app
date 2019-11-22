88ce83d330b65ecdc3ab7297803e3d3876f38908
arthur-nesterenko <arthur-nesterenko@users.noreply.github.com>
2019-11-22 12:22:53 +0000

    Automated deployment: Fri Nov 22 12:22:53 UTC 2019 bb8ee83a0be33670d565e08eae7e1b7b8e2d985b

---
+++ precache-manifest.cb7211360ba09b43b899c5fa5ee27a24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9192f4c5740d976155eb1f4fce682b",
+    "url": "/simple-scheduling-app/index.html"
+  },
+  {
+    "revision": "3f1550b69ae4a33f485f",
+    "url": "/simple-scheduling-app/static/css/2.d32fc048.chunk.css"
+  },
+  {
+    "revision": "3f1550b69ae4a33f485f",
+    "url": "/simple-scheduling-app/static/js/2.de601c00.chunk.js"
+  },
+  {
+    "revision": "783d2a38a0595ff881f6",
+    "url": "/simple-scheduling-app/static/js/main.e9adc34b.chunk.js"
+  },
+  {
+    "revision": "e7baee67b87c0b7838b6",
+    "url": "/simple-scheduling-app/static/js/runtime-main.e557a511.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e7ddcb91c570d859623cc6034017d9ac.js"
+  "/simple-scheduling-app/precache-manifest.cb7211360ba09b43b899c5fa5ee27a24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-scheduling-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
